03a3a05a9f933db8f1228a6950f4c0b40587aa0c
yonguk <kimyu9827@naver.com>
2023-12-05 14:28:45 +0900

    feat:
    home controller
    ??

--- src/main/java/com/ll/medium/MemberController.java
+++ src/main/java/com/ll/medium/MemberController.java
@@ -1,8 +1,13 @@
 package com.ll.medium;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
 
+    @GetMapping
+    public void home(){
+
+    }
 }
