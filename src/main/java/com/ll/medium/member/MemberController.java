b2f854c1a41b724374902767a43c559395c856dc
yonguk <kimyu9827@naver.com>
2023-12-06 10:17:24 +0900

    feat: join
    로그인 기능 구현 완료

--- src/main/java/com/ll/medium/member/MemberController.java
+++ src/main/java/com/ll/medium/member/MemberController.java
@@ -2,6 +2,7 @@ package com.ll.medium.member;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,8 +27,28 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String join(@Valid MemberCreateForm memberCreateForm, BindingResult buildingResult){
+    public String join(@Valid MemberCreateForm memberCreateForm, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return "signup_form";
+        }
 
-        return "";
+        if (!memberCreateForm.getPassword().equals(memberCreateForm.getPasswordConfirm())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "signup_form";
+        }
+        try {
+            memberService.create(memberCreateForm);
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/ll/medium/member/MemberService.java
+++ src/main/java/com/ll/medium/member/MemberService.java
@@ -1,7 +1,21 @@
 package com.ll.medium.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void create(MemberCreateForm memberCreateForm){
+        Member member = Member.builder()
+                .username(memberCreateForm.getUsername())
+                .password(memberCreateForm.getPassword())
+                .build();
+
+        memberRepository.save(member);
+    }
+
 }
