aeff97a8c176c53b166d118a03df4f74865c64e3
yonguk <kimyu9827@naver.com>
2024-01-08 14:20:20 +0900

    remove: frontend

--- frontend/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/api/v1/post/[articleId]/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/api/v1/post/[articleId]/+page.svelte";
--- frontend/.svelte-kit/generated/client/nodes/4.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/api/v1/post/list/+page.svelte";
