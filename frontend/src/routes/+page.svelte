098a07a553343bd03bc07f536a23c2494e05e6c9
yonguk <kimyu9827@naver.com>
2024-01-08 14:43:15 +0900

    init: front folder, tailwind, daisyui

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form>
+    <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+</form>
