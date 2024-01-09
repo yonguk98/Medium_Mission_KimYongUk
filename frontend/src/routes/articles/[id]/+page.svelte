f262f919d50107c9077f6c4eb6ed3554cef2c4c8
yonguk <kimyu9827@naver.com>
2024-01-10 00:23:10 +0900

    feature: show  comment and modify, delete button

--- frontend/src/routes/articles/[id]/+page.svelte
+++ frontend/src/routes/articles/[id]/+page.svelte
@@ -1,13 +1,16 @@
 <script>
     import {onMount} from "svelte";
     import {page} from "$app/stores";
+    import Time from "svelte-time";
 
     let article = [];
+    let comments = [];
     onMount(async ()=>{
         await fetch(`http://localhost:8080/api/v1/post/`+$page.params.id)
             .then((res)=> res.json())
             .then((res)=>{
                 article = res;
+                comments = article.commentList;
                 console.log(article);
             })
     })
@@ -20,9 +23,22 @@
     </div>
 </div>
 
+{#each comments as comment}
 <div class="card w-auto bg-base-100 shadow-xl m-1">
     <div class="card-body">
-        <h2 class="card-title">{article.commentList}</h2>
-        <p>{article.body}</p>
+        <div class="columns-2">
+            <p>{comment.body}</p>
+            <div class="text-end">
+                <span>
+                    <button class="btn btn-square btn-primary">수정</button>
+                    <button class="btn btn-square btn-primary">삭제</button>
+                </span>
+            </div>
+        </div>
+        <div class="text-end">
+            <Time timestamp="{comment.dateTime}" format="MMM DD YYYY, hh:mm"></Time>
+        </div>
+
     </div>
 </div>
+{/each}
