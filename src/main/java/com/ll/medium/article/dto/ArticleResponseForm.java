2b64393f54c85ae21cce7bfe886849a4deb7d47e
yonguk <kimyu9827@naver.com>
2024-01-09 22:55:57 +0900

    feature: create article response form

--- src/main/java/com/ll/medium/article/dto/ArticleResponseForm.java
+++ src/main/java/com/ll/medium/article/dto/ArticleResponseForm.java
@@ -1,4 +1,29 @@
-package com.ll.medium.article.entity;
+package com.ll.medium.article.dto;
 
+import com.ll.medium.article.entity.Article;
+import com.ll.medium.comment.entity.Comment;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.annotation.processing.Generated;
+import java.util.List;
+
+@Getter
+@Setter
 public class ArticleResponseForm {
+    private String title;
+    private String body;
+    private String writer;
+    private Long hit;
+    private Integer like;
+    private List<Comment> commentList;
+
+    public ArticleResponseForm(Article article){
+        this.title = article.getTitle();
+        this.body = article.getBody();
+        this.writer = article.getWriter();
+        this.hit = article.getHit();
+        this.like = article.getLike().size();
+        this.commentList = article.getCommentList();
+    }
 }
